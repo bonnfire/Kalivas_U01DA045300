98568cec673c3b54367fc482b198f5fe966d0c5c
Bonnie Lin <b2lin@ucsd.edu>
2019-11-21 16:22:53 -0800

    extracting kalivas, include naniar::vis_miss to determine complete.cases function use, change the varnames to be uniform and unique, create total dataset and individual

--- CREATE/CREATE_EXCEL_KALIVAS.R
+++ CREATE/CREATE_EXCEL_KALIVAS.R
@@ -35,10 +35,35 @@ for(i in 1:length(openfieldtask_files)){
     assign(gsub("\\.xlsx", "", sapply(strsplit(openfieldtask_files, "/"), "[", 3))[i], u01.importxlsx(openfieldtask_files[i]))
 } # creates multiple objects for every excel file
 
+ls()[sapply(ls(), function(i) class(get(i))) == "list"] # use this vector of these objects to make them into df's to be bound together in to a data frame cohort 2
+
+cohort2 <- list() #all that contain cohort2)
+
+  
+cohort02_group1_OF1_test <- list()
+# change the variable names # extract the data and create the summary as a separate list item
+names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1) <- ifelse(grepl("^[[:alpha:]].*\\d$", names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1)), gsub("[.].*", "", names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1)), names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1)) 
+
+names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1) <- mgsub::mgsub(names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1),c("-| "), c("_")) %>% 
+  tolower() %>% 
+  make.unique(sep = ".")
+  
+
+cohort02_group1_OF1_test[[1]] <- cohort02_group1_OF1$C2Group1OF1_C2G1OF1 %>% 
+                           select(ends_with(".1")) %>% # creates the "total" table
+                            dplyr::filter(complete.cases(.))
+cohort02_group1_OF1_test[[2]] <- cohort02_group1_OF1$C2Group1OF1_C2G1OF1 %>% 
+  select(-matches("\\d$"))
+naniar::vis_miss(cohort02_group1_OF1_test[[2]])
+cohort02_group1_OF1_test[[2]] <- cohort02_group1_OF1$C2Group1OF1_C2G1OF1 %>% 
+  select(-matches("\\d$")) %>%  # matches is the only select helper that allows for regular expressions
+  dplyr::filter(complete.cases(.)) %>% 
+  select(subject_id, everything()) # reorder the columns 
+# todo: change the date object. check the number of unique id matches expected values.
 
 
 
-
+  
 ############################
 # Exp 3: TAIL FLICK
 ############################
