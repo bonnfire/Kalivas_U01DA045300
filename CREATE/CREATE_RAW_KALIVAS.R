1d054e07ca9b529f050310890580e272d7cf00d9
Bonnie Lin <b2lin@ucsd.edu>
2019-11-22 13:25:53 -0800

    begin cleaning the files, change the var name to be uniform, define a new variable to later test the vmx values

--- CREATE/CREATE_RAW_KALIVAS.R
+++ CREATE/CREATE_RAW_KALIVAS.R
@@ -4,6 +4,28 @@
 
 setwd("~/Dropbox (Palmer Lab)/Peter_Kalivas_U01/behavioral_tasks")
 
+
+# self defined functions
+uniform.var.names.testingu01 <- function(df){
+  lapply(seq_along(df), function(i) {
+    if(grepl("Parent", names(df[[i]])) %>% any()){
+      names(df[[i]])[1:2] <- df[[i]][1,][1:2] %>% as.character()
+      df[[i]] <- df[[i]][-1, ]
+    }
+    names(df[[i]]) <- mgsub::mgsub(names(df[[i]]),
+                                   c(" |\\.", "#", "Transponder ID", "Date of Wean|Wean Date","Animal", "Shipping|Ship", "Dams"),
+                                   c("", "Number", "RFID", "DOW","LabAnimal", "Shipment", "Dames"))
+    # names(df[[i]]) <- mgsub::mgsub(names(df[[i]]),
+    #                                c("DateofShipment", "LabAnimalID"), 
+    #                                c("ShipmentDate", "LabAnimalNumber"))
+    names(df[[i]]) <- mgsub::mgsub(names(df[[i]]),
+                                   c("DateofShipment", "LabAnimalNumber"), 
+                                   c("ShipmentDate", "LabAnimalID")) # actually keep the column named lab animal number
+    names(df[[i]]) <- tolower(names(df[[i]]))
+    df[[i]]
+  })
+}
+
 ############################
 # Exp 1: ELEVATED PLUS MAZE
 ############################
@@ -16,6 +38,7 @@ setwd("~/Dropbox (Palmer Lab)/Peter_Kalivas_U01/behavioral_tasks/elevated_plus_m
 # Exp 2: OPEN FIELD TASK
 ############################
 setwd("~/Dropbox (Palmer Lab)/Peter_Kalivas_U01/behavioral_tasks/open_field_task")
+# cohort 2 and 3 excel and ACT files
 openfieldtask_files_raw <- list.files(path = ".", pattern = "*.ACT", full.names = T, recursive = T) 
 
 read_oft <- function(x){
@@ -24,9 +47,23 @@ read_oft <- function(x){
   return(read_oft)
 }
 
-openfieldtask_raw <- lapply(openfieldtask_files_raw, read_oft)
-# cohort 2 and 3 excel and ACT files
+openfieldtask_raw_list <- lapply(openfieldtask_files_raw, read_oft)
+names(openfieldtask_raw_list) = openfieldtask_files_raw
 
+openfieldtask_raw_list <- lapply(openfieldtask_raw_list, function(df){
+  df <- df %>% 
+    select(-V38) %>%     
+    mutate(vmx = ifelse(grepl("^\\d", df$CAGE), NA, df$CAGE)) %>% 
+    dplyr::filter(grepl("^\\d", CAGE)) %>% 
+    mutate(CAGE = as.numeric(CAGE)) %>%  
+    arrange(CAGE)
+  return(df)
+  }) %>% uniform.var.names.testingu01()
+# naniar::vis_miss(rbindlist(openfieldtask_raw_list, fill = T)) nothing abnormal; remove V38 bc all are empty; check why some columns are 100% empty and if the kalivas lab kept these
+openfieldtask_raw_df <- rbindlist(openfieldtask_raw_list, fill = T)
+
+
+# todo: finish completing the extraction and date/time formatting, check that the names are the same as the excel file
 
 ############################
 # Exp 3: TAIL FLICK
