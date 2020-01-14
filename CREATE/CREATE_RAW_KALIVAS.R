1a7e23095c64e8fbf2d895d3ff1acc669f987ea7
Bonnie Lin <b2lin@ucsd.edu>
2020-01-13 17:41:25 -0800

    extract M, O, P, and B array from PR files

--- CREATE/CREATE_RAW_KALIVAS.R
+++ CREATE/CREATE_RAW_KALIVAS.R
@@ -110,6 +110,68 @@ allcohorts_files <- dcast(allcohorts_df_nodupes, formula = subject ~ experiment,
          "Extinction 4", "Extinction 5", "Extinction 6", "Cued rein")
 write.xlsx(allcohorts_files, "allcohorts_df_count.xlsx")
 
+# *****************
+
+
+##  PR_test
+allcohorts_pr_fnames <- grep("progressive", allcohorts_allexp_filenames, ignore.case = T, value = T) #13 files
+
+# Extract subject information
+pr_subjects <- lapply(allcohorts_pr_fnames, readsubject) %>% 
+  rbindlist(fill = T) %>% 
+  rename("subjectid"= "V1") %>% 
+  group_by(filename) %>% 
+  mutate(numseq = row_number()) %>% 
+  ungroup() 
+pr_subjects %>% dplyr::filter(is.na(subjectid)) # check for no na
+
+# P array contains the break points and the O value contains the PR step at which the rat terminated at to give PR_step; 
+# M contains the totaL_session_minutes; and B array contains  inactive lever, active lever, infusion, and current ratio information
+# readParray <- function(x){
+#   Parray <- fread(paste0("awk '/P:/{flag=1;next}/S:/{flag=0}flag' ", "'", x, "'"), header = F, fill = T)
+#   Parray$filename <- x
+#   return(Parray)
+# } ## checkin with Apurva 
+readBarray <- function(x){
+  Barray <- fread(paste0("grep -a1 --no-group-separator -En '(B):' ", "'", x, "'", " | grep -E '( 0):'"), header = F, fill = T)
+  Barray$filename <- x
+  return(Barray)
+} 
+
+readM_O <- function(x){
+  M_O <- fread(paste0("grep -a1 --no-group-separator -En '(M|O):' ", "'", x, "'", " | grep -E '(M|O):'"), header = F, fill = T)
+  M_O$filename <- x
+  return(M_O)
+} 
+
+
+pr_Parray <- lapply(allcohorts_pr_fnames, readParray) %>% rbindlist(fill = T)  
+pr_Parray_indices <- grep("^0:$", pr_Parray$V1)
+split_Parray <- split(pr_Parray, cumsum(1:nrow(pr_Parray) %in% pr_Parray_indices))
+processeddata <- lapply(split_Parray, function(x){
+  indexremoved <- x %>% select(-V1)
+  Parray <- as.vector(t(data.matrix(indexremoved)))
+  Parray <- Parray[!is.na(Parray)]
+  Parray <- prepend(Parray, 1) #PR list with each break point listed, expect for the first breakpoint, which is 1. -MedPC column descriptions (cohort2-- could be an issue for other cohorts- checkXX )
+  return(Parray)
+})
+
+pr_Barray <- lapply(allcohorts_pr_fnames, readBarray) %>% rbindlist(fill = T) %>% 
+  select(-c(V2, V7)) %>% 
+  dplyr::rename("rownum" = "V1",
+              "inactive_lever" = "V3",
+              "active_lever" = "V4", 
+              "infusions" = "V5",
+              "current_ratio" = V6) %>% 
+  mutate(rownum = gsub("-", "", rownum) %>% as.numeric) %>% 
+  arrange(filename, rownum)
+pr_M_O <- lapply(allcohorts_pr_fnames, readM_O) %>% rbindlist(fill = T) %>% 
+  tidyr::separate(V1, c("rownum", "var"), sep = ":") %>% 
+  rename("value" = "V2") 
+
+
+pr_M_O %>% dplyr::filter(var == "O")
+
 
 
 
