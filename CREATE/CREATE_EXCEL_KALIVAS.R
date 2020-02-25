f5af9632e05d0dee78741302809939d639c00a2a
Bonnie Lin <b2lin@ucsd.edu>
2020-02-24 18:36:45 -0800

    extract the oft data from excel

--- CREATE/CREATE_EXCEL_KALIVAS.R
+++ CREATE/CREATE_EXCEL_KALIVAS.R
@@ -204,8 +204,9 @@ extract_process_excel_expr_lapply <- function(files, sheet){
   return(data_breeder_list)
 }
 
-kalivas_expr_allcohorts_excel_processed <- extract_process_excel_expr_lapply(all_excel_fnames, "extinction_prime_test") %>% rbindlist()
 
+
+kalivas_expr_allcohorts_excel_processed <- extract_process_excel_expr_lapply(all_excel_fnames, "extinction_prime_test") %>% rbindlist()
 
 # *****************
 ##  Extinction
@@ -218,6 +219,39 @@ kalivas_cued_allcohorts_excel_processed <- extract_process_excel_shortened_lappl
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ############################
 # # Exp 1: ELEVATED PLUS MAZE
 # ############################
@@ -226,100 +260,58 @@ kalivas_cued_allcohorts_excel_processed <- extract_process_excel_shortened_lappl
 # # all wmv files
 # 
 # 
-# ############################
-# # Exp 2: OPEN FIELD TASK
-# ############################
-# setwd("~/Dropbox (Palmer Lab)/Peter_Kalivas_U01/behavioral_tasks/open_field_task")
-# 
-# # cohort 2 and 3 excel and ACT files
-# 
-# # read in the excel files 
-# openfieldtask_files_excel <- list.files(path = ".", pattern = "*.xlsx", full.names = T, recursive = T) 
-# openfieldtask_files_excel <- grep("cohort", openfieldtask_files_excel, ignore.case = T, value = T) # ignore the other xlsx file
-# 
-# # for(i in 1:length(openfieldtask_files)){
-# #     assign(gsub("\\.xlsx", "", sapply(strsplit(openfieldtask_files_excel, "/"), "[", 3))[i], u01.importxlsx(openfieldtask_files[i]))
-# # } # creates multiple objects for every excel file
-# 
-# ls()[sapply(ls(), function(i) class(get(i))) == "list"] # use this vector of these objects to make them into df's to be bound together in to a data frame cohort 2
-# 
-# 
-# # create df with all excel values
-# # not needed sapply(strsplit(openfieldtask_files_excel, "/"), "[", 3)
-# openfieldtask_excel_df <- list()
-# openfieldtask_excel_list <- lapply(openfieldtask_files_excel, read_excel)
-# # since sapply(openfieldtask_excel_df, length) is all 1's, we won't need to use the u01.importxlsx function; sapply(openfieldtask_excel_list, dim)[2,] %>% summary shows that all dataframes have 46 vars
-# names(openfieldtask_excel_list) <- str_match(openfieldtask_files_excel, "/.*/(.*?)\\.")[,2] ## XX what's up with this case "cohort03_subject_104_and_108_to_110)_OF2"
-# 
-# # naniar::vis_miss(rbindlist(openfieldtask_excel_list, fill = T)) there is some missingness that we should probably address but we can leave that for another time
-# openfieldtask_excel_list_test <- lapply(openfieldtask_excel_list, function(x){
-#   names(x) <-ifelse(grepl("^[[:alpha:]].*\\d$", names(x)), gsub("[.].*", "", names(x)), names(x)) # remove trailing dots and variable numbers
-#   names(x) <- mgsub::mgsub(names(x),c("-| "), c("_")) %>% 
-#     tolower() %>% 
-#     make.unique(sep = ".")
-#   x %<>% mutate(date = as.POSIXct(strptime(date,format="%d-%b-%Y")),
-#          time = chron::chron(times = time))
-#   return(x)
-# })
-# 
-# # create the total tables extracted from the excel files
-# openfieldtask_excel_df_total <- lapply(openfieldtask_excel_list_test, function(x){
-#  x <- x %>% 
-#     select(ends_with(".1")) %>% 
-#     dplyr::filter(complete.cases(.))
-#  return(x)
-# }) %>% rbindlist(fill = T, idcol = "actfile")
-# 
-# openfieldtask_excel_df_data <- lapply(openfieldtask_excel_list_test, function(x){
-#   x <- x %>% 
-#     select(-matches("\\d$")) %>% 
-#     dplyr::filter(grepl("^\\d", x$cage))
-#   return(x)
-# }) %>% rbindlist(fill = T, idcol = "cohort") %>% 
-#   mutate(cohort = gsub("_.*", "", cohort),
-#          cage = as.numeric(cage),
-#          tempdegc = as.logical(tempdegc),
-#          wheelrot = as.logical(wheelrot))
-# 
-# openfieldtask_excel_df_data[which(is.na(openfieldtask_excel_df_data$subject_id)),] ## XX found missing labanimalid - easy fix though, just need confirmation that i can
-# 
-# 
-# 
-# 
-# 
-# 
-# # test for one case, eventually use this to create the cohort objects above 
-# # change the variable names # extract the data and create the summary as a separate list item
-# names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1) <- ifelse(grepl("^[[:alpha:]].*\\d$", names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1)), gsub("[.].*", "", names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1)), names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1)) 
-# 
-# names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1) <- mgsub::mgsub(names(cohort02_group1_OF1$C2Group1OF1_C2G1OF1),c("-| "), c("_")) %>% 
-#   tolower() %>% 
-#   make.unique(sep = ".")
-#   
-# cohort02_group1_OF1_test <- list()
-# cohort02_group1_OF1_test[[1]] <- cohort02_group1_OF1$C2Group1OF1_C2G1OF1 %>% 
-#                            select(ends_with(".1")) %>% # creates the "total" table
-#                             dplyr::filter(complete.cases(.)) 
-# cohort02_group1_OF1_test[[2]] <- cohort02_group1_OF1$C2Group1OF1_C2G1OF1 %>% 
-#   select(-matches("\\d$")) %>% 
-#   mutate(date = as.Date(date, format='%d-%b-%Y') ,
-#          time = chron::chron(times = time)) 
-#   
-# naniar::vis_miss(cohort02_group1_OF1_test[[2]]) # rather than visualizing the graph every time, # consider coding this ifelse in the future: return a comment if the percentages diff
-# 
-# cohort02_group1_OF1_test[[2]] <- cohort02_group1_OF1$C2Group1OF1_C2G1OF1 %>% 
-#   select(-matches("\\d$")) %>%  # matches is the only select helper that allows for regular expressions
-#   dplyr::filter(complete.cases(.)) %>% 
-#   select(subject_id, everything()) %>%  # reorder the columns 
-#   mutate(date = as.Date(date, format='%d-%b-%Y') ,
-#          time = chron::chron(times = time))
-# 
-# nrow(cohort02_group1_OF1_test[[2]]) == n_distinct(cohort02_group1_OF1_test[[2]]$subject_id)
-# # check the number of unique id matches expected values.
-# 
-# 
-# 
-#   
+
+############################
+# Exp 2: OPEN FIELD TASK
+############################
+
+
+extract_process_excel_repeatedmeasures2_lapply <- function(files, sheet){ # for use on before self admin and after self admin
+  data_breeder_list <-  lapply(files, function(i) {
+    data_allsheets = u01.importxlsx(i)
+    data_breeder <- data_allsheets[[sheet]] # made to extract any of the sheets
+    
+    databegins_index <- which(data_breeder[, 8] == "Date")
+    
+    names(data_breeder)[8:ncol(data_breeder)] <- data_breeder[databegins_index, 8:ncol(data_breeder)] %>% unlist() %>% as.character()
+    make_unique = function(x, sep='_'){
+      ave(x, x, FUN=function(a){if(length(a) > 1){paste(a, 1:length(a), sep=sep)} else {a}})
+    }
+    names(data_breeder) <- names(data_breeder) %>% gsub(" ", "", .) %>% tolower() %>% make_unique()
+    df_values <- data_breeder[(databegins_index + 1):nrow(data_breeder),]
+    df <- df_values %>%
+      dplyr::filter(!is.na(microchip)) %>%  ## okay doing this bc all other data_breeder na
+      mutate_at(vars(matches("date")), openxlsx::convertToDateTime, origin = "1900-01-01") %>% 
+      mutate_at(vars(matches("date")), as.character) %>% 
+      gather(var, value, -microchip, -sex, -bx_unit, -cohort_number, -internal_id, -group, -heroin_or_saline) %>%
+      extract(var, c("measurement", "session"), "(\\D+_?\\d?)_(\\d)") %>% 
+      mutate(session = ifelse(session == 1, "before_SA", "after_SA"))  %>%
+      spread(measurement, value) %>% 
+      mutate(cohort_number = gsub("MUSC_", "", cohort_number)) %>%
+      rename("rfid" = "microchip")
+    return(df)
+    
+  })
+  return(data_breeder_list)
+}
+
+kalivas_oft_allcohorts_excel_processed <- extract_process_excel_repeatedmeasures2_lapply(all_excel_fnames, "open_field") %>% rbindlist()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ############################
 # # Exp 3: TAIL FLICK
 # ############################
