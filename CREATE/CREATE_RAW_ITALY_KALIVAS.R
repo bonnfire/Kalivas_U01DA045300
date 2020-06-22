5fc1098cfb21e50022aebeb184d8dd2142b3ffbd
Bonnie Lin <b2lin@ucsd.edu>
2020-06-22 14:19:40 -0700

    extract long access information and begin assigning subjects

--- CREATE/CREATE_RAW_ITALY_KALIVAS.R
+++ CREATE/CREATE_RAW_ITALY_KALIVAS.R
@@ -28,6 +28,61 @@ read.inactive <- function(x){
   return(R_array)
 } 
 
+### function to expand the subject names in filename 
+## functions for wrangling
+## create new rows to expand on the projects 
+expand.project.dash <- function(txt) {
+  
+  if((grepl("\\d[[:space:]]?-[[:space:]]?\\d", txt) & !grepl("Subject", txt, ignore.case = T))){
+    str <- gsub('[(]\\d+\\s?-\\s?\\d+[)](.txt)?', '%s', txt)
+    dashed_str <- gsub('[a-zA-Z ()]+', '', txt)
+
+    expand.dash <- function(dashed) {
+      limits <- as.numeric(unlist(strsplit(dashed, '-')))
+      first <- seq(limits[1], limits[2], 4)
+      second <- seq(limits[1], limits[2], 4) + 1
+      c(rbind(first, second))
+    }
+
+    paste0(sprintf(str, expand.dash(dashed_str)), sep = "", collapse = ",")
+  }
+  else if(grepl("\\d[[:space:]]?-[[:space:]]?\\d", txt) & grepl("Subject", txt, ignore.case = T)){
+    paste0(gsub("([MF])[(].*", "\\1", txt), parse_number(gsub(".*Subject ", "", txt)))
+  } 
+  else{
+    paste0(txt)
+  }
+}
+expand.project.dash <- Vectorize(expand.project.dash)
+
+##################################################
+##################################################
+## LONG ACCESS SELF ADMIN
+##################################################
+##################################################
+
+########### INACTIVE LEVER PRESSES (LP), ACTIVE LP, AND INFUSIONS
+read.lga <- function(x){
+  lga <- fread(paste0("awk '/L:/{flag=1;next}/^$/{flag=0}flag' ", "'", x, "'", " | awk '/ 0:/{print $2}'"), header = F, fill = T)
+  # lga$filename <- x
+  lga_df <- as.data.frame(matrix(lga$V1, ncol = 3))
+  lga_df$filename <- x
+  lga_df <- lga_df %>% 
+    mutate(subject = str_extract(filename, "[MF](.*+)")) %>% 
+    mutate(subject = expand.project.dash(subject))
+  
+  return(lga_df)
+  # return(lga)
+  }
+
+primedrein_active <- lapply(grep("long-access", raw_filenames_italy_kalivas, value = T, ignore.case = T), read.lga) %>% 
+  rbindlist(fill = T) %>% 
+  rename("row_number"="V1") %>% 
+  select(row_number, filename) %>% 
+  separate(row_number, c("row_number", "lever_presses"), sep = "_") %>% 
+  mutate(lever_presses = gsub(",", ".", lever_presses))
+
+
 
 ##################################################
 ##################################################
