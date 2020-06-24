ff5e8072f7e9b0fa6f1ade446fecab6810946d09
Bonnie Lin <b2lin@ucsd.edu>
2020-06-23 17:04:50 -0700

    first hour extraction function

--- CREATE/CREATE_RAW_ITALY_KALIVAS.R
+++ CREATE/CREATE_RAW_ITALY_KALIVAS.R
@@ -60,9 +60,11 @@ expand.project.dash <- Vectorize(expand.project.dash)
 
 ##################################################
 ##################################################
-## LONG ACCESS SELF ADMIN
+## LONG ACCESS SELF ADMIN (12 HOUR AND 1 HOUR)
 ##################################################
 
+
+## 12 HOURS 
 ########### INACTIVE LEVER PRESSES (LP), ACTIVE LP, AND INFUSIONS
 read.lga <- function(x){
   lga <- fread(paste0("awk '/Group:/{flag=1;next}/^$/{flag=0}flag' ", "'", x, "'", " | awk '/Box:| 0:/{print $1, $2}'"), header = F, fill = T)
@@ -100,6 +102,60 @@ lga_raw_df <- lga_raw_df %>%
          exp = gsub(".*(LgA\\d+).*", "\\1", filename, ignore.case = T),
          saline = ifelse(grepl("saline", filename, ignore.case = T), "Saline", NA))
 
+
+#### FIRST(1) HOUR 
+
+## extract the infusions, active, and inactive lever (first 12 5 minute bins)
+read.selfadmin.firsthour <- function(x){
+  lga_firsthour <- fread(paste0("grep -A 3 -E \"(Box|L|R|W):\" --no-group-separator ", "'", x, "'", "| grep -v \"MSN:\" "), header = F, fill = T)
+  lga_firsthour_split <- split(lga_firsthour, findInterval(1:nrow(lga_firsthour), grep("Box:", lga_firsthour$V2)))
+  # maxlength <- lga_firsthour_split %>% sapply(nrow) %>% max #get the max number of "arrays"
+  lga_firsthour_merge <- lapply(lga_firsthour_split, function(x){
+    x <- x %>% 
+      mutate(V2 = replace(V2, V1=="0:", NA)) %>%  # remove the total 12h value, remove any 5 min bins after an hour (12th bin)
+      mutate_at(vars(matches("V[456]")), ~ replace(., V1=="10:", NA) ) 
+    
+    # splitting the array 
+    x_split <- split(x, findInterval(1:nrow(x), grep("(L|R|W):", x$V1)))
+    
+    x_split_2 <- lapply(x_split, function(x){
+      x <- x %>% 
+        mutate(V1 = replace(V1, grepl("(0|5|10):", V1), NA)) %>% 
+        fill(V1) %>% 
+        mutate_all(as.character) %>%
+        mutate_at(vars(-matches("filename")), ~ gsub(",", ".", .)) %>% 
+        mutate(V7 = paste0(c(V2, V3, V4, V5, V6), collapse = ",") ) %>% 
+        group_by(V1) %>% 
+        mutate(V8 = paste0(V7, collapse = ",") ) %>% 
+        ungroup() %>% 
+        subset(grepl("^$|Time", V2)|grepl("Box", V1)) %>% 
+        separate(V8, into = c(paste0("C", 1:15)), sep = ",") %>% 
+        select(-V7) %>% 
+        mutate_at(vars(matches("C\\d+")), ~ replace(., !grepl("[.]00", .), NA)) %>% 
+        mutate_at(vars(matches("C([3-9]|1[0-5])")), ~ replace(., V1 == "W:", NA)) %>% 
+        mutate_at(vars(matches("C\\d+")), as.numeric) %>% 
+        mutate(sum1 = rowSums(.[grep("C\\d+", names(.))], na.rm = TRUE))
+      return(x)
+    }) %>% rbindlist(fill = T)
+    
+    return(x_split_2)
+    }) %>%
+    rbindlist(fill = T) 
+  # %>%
+  #   mutate(filename = x)
+  return(lga_firsthour_merge)
+  # return(lga_firsthour)
+}
+
+
+# lga_firsthour_raw <- 
+  lapply(grep("long-access", raw_filenames_italy_kalivas, value = T, ignore.case = T)[280], read.selfadmin.firsthour) %>%
+  rbindlist(fill = T) 
+
+
+
+
+
 ##################################################
 ##################################################
 ## PRIMED REINSTATEMENT
@@ -124,6 +180,53 @@ primedrein_inactive <- lapply(grep("primed rein", raw_filenames_italy_kalivas, v
   select(row_number, filename) %>% 
   separate(row_number, c("row_number", "lever_presses"), sep = "_") %>% 
   mutate(lever_presses = gsub(",", ".", lever_presses))
+
+
+##################################################
+##################################################
+##  CUED REINSTATEMENT
+##################################################
+##################################################
+
+########### INACTIVE LEVER PRESSES (LP), ACTIVE LP, AND PUMP ACTIVATIONS
+read.relapse <- function(x){
+  cued_rein <- fread(paste0("awk '/Group:/{flag=1;next}/^$/{flag=0}flag' ", "'", x, "'", " | awk '/Box:| 0:/{print $1, $2}'"), header = F, fill = T)
+  cued_rein_split <- split(cued_rein, findInterval(1:nrow(cued_rein), grep("Box:", cued_rein$V1)))
+  maxlength <- cued_rein_split %>% sapply(nrow) %>% max #get the max number of "arrays"
+  cued_rein_merge <- lapply(cued_rein_split, function(x, maxlength){
+    arrays <- x$V2
+    length(arrays) <- maxlength 
+    as.data.frame(matrix(arrays, ncol = maxlength, byrow = T))
+  }, maxlength = maxlength) %>% 
+    rbindlist(fill = T) %>%
+    mutate(filename = x)
+  
+  # cued_rein_df <- cued_rein_df %>% 
+  #   mutate(subject = str_extract(filename, "[MF](.*+)")) %>% 
+  #   mutate(subject = expand.project.dash(subject))
+  
+  return(cued_rein_merge)
+  # return(cued_rein)
+}
+
+relapse_raw <- lapply(grep("cued", raw_filenames_italy_kalivas, value = T, ignore.case = T), read.relapse) %>%
+  rbindlist(fill = T) 
+names(relapse_raw) <- c("box", "A", "inactive_presses", "active_presses", "pump_activation", "filename")
+relapse_raw_df <- relapse_raw %>% 
+  mutate_all(as.character) %>%
+  mutate_at(vars(-matches("filename")), ~ gsub(",", ".", .))
+
+relapse_raw_df %>% mutate(box = as.numeric(box)) %>% subset(box>10) %>% dim
+
+relapse_raw_df <- relapse_raw_df %>% 
+  mutate(sex = ifelse(grepl("[MF][(]", filename, ignore.case = T), gsub(".*([MF])[(].*", "\\1", filename), NA),
+         cohort = str_pad(parse_number(gsub(".*unicam_cohort_(\\d+)[/].*", "\\1", filename)), 2, "left", "0"),
+         exp = gsub(".*(relapse\\d+).*", "\\1", filename, ignore.case = T),
+         saline = ifelse(grepl("saline", filename, ignore.case = T), "Saline", NA))
+
+
+
+
 
 
 ##################################################
